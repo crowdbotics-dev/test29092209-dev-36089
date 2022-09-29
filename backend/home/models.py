abb8b5d805964b9ec1096939827d5e4a979cb33f
Crowdbotics <engineering@crowdbotics.com>
2022-09-29 11:14:29 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+
+
+class Login(models.Model):
+    "Generated Model"
+    asdfh = models.PositiveSmallIntegerField()
+    yhfjh = models.BigIntegerField()
--- backend/test29092209_dev_36089/wsgi.py
+++ backend/test29092209_dev_36089/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test29092209_dev_36089.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test29092209_dev_36089.settings")
 
 application = get_wsgi_application()
